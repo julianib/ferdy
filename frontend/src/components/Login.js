d301cac5194d821e9bd503f85be0a29a04be8599
itsbadr <badrkmc@gmail.com>
2021-12-10 22:18:56 +0500

    login and logout buttons

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,33 +1,54 @@
-import { GoogleLogin } from "react-google-login";
+import { useState } from "react";
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 export default function Login() {
 
-    async function onSuccess(res) {
-        console.log("Login successful", res.profileObj)
-        fetch("http://localhost:3001", {
-            method: "POST",
-            mode: "cors",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(res.profileObj)
-        });
-    }
+  const CLIENT_ID = "179923541658-kfl4lp6lgd1nur0pk5vnqsb3d2hg49e6.apps.googleusercontent.com";
+  const [user, setUser] = useState(null);
 
-    function onFaliure(res) {
-        console.log("Login unsuccessful", res);
-    }
+  async function onSuccess(res) {
+    setUser(res.profileObj);
 
-    return (
-        <>
-            <h1>Login</h1>
-            <GoogleLogin
-                clientId="179923541658-kfl4lp6lgd1nur0pk5vnqsb3d2hg49e6.apps.googleusercontent.com"
-                buttonText="Login"
-                onSuccess={onSuccess}
-                onFailure={onFaliure}
-                cookiePolicy={'single_host_origin'}
-            />
-        </>
-    )
+    fetch("http://localhost:3001", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ tokenId: res.tokenId })
+    });
+  }
+
+  function onFaliure(res) {
+    console.log("Login unsuccessful", res);
+  }
+
+  function logout() {
+    setUser(null)
+  }
+
+  return (
+    <>
+      {user ? (
+        <div>
+          <h1>Welcome, {user.givenName}</h1>
+
+          <GoogleLogout
+            clientId={CLIENT_ID}
+            buttonText={"Logout"}
+            onLogoutSuccess={logout}
+          ></GoogleLogout>
+        </div>
+      ) : (
+        <GoogleLogin
+          clientId={CLIENT_ID}
+          buttonText="Sign In with Google"
+          onSuccess={onSuccess}
+          onFailure={onFaliure}
+          isSignedIn={true}
+          cookiePolicy={"single_host_origin"}
+        />
+      )}
+    </>
+  )
 }
