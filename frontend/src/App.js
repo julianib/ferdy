d6069eae02a9cc8502512c67e0d1ddb478f8a295
Julian <julianibibo@gmail.com>
2021-12-24 20:29:28 +0100

    cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,9 @@
 import Login from "./components/Login";
 import ChatBox from "./components/ChatBox";
-import { useContext, useEffect, useState } from "react";
-import useChatMessages from "./hooks/useChatMessages";
-import ChatMessage from "./components/ChatMessage";
-import { UserContext } from "./contexts/UserContext";
+import useUser from "./hooks/useUser";
 
 export default function App() {
-  // const { user } = useUser(); // broken wtffffffff
-  const { user } = useContext(UserContext);
-  const [test, setTest] = useState(0);
-  const { chatMessages } = useChatMessages();
-
-  useEffect(() => {
-    console.debug("useEffect App", user);
-  });
+  const { user } = useUser();
 
   return (
     <>
@@ -21,19 +11,6 @@ export default function App() {
       <Login />
       <ChatBox />
       {user?.name || "no user set"}
-      <button
-        onClick={() => {
-          setTest((oldState) => oldState + 1);
-        }}
-      >
-        click to update App(){test}
-      </button>
-      <br />
-      <ul>
-        {chatMessages.map((chatMessage, i) => {
-          return <ChatMessage key={i} {...chatMessage} />;
-        })}
-      </ul>
     </>
   );
 }
